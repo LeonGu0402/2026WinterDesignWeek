04333400ac1f2e5c8d707ef5fd4ff7474fa06223
thunderfern <115232330+Stwikey@users.noreply.github.com>
2026-03-05 13:46:45 -0500

    New Audio manager with music

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -15,8 +15,72 @@ public class AudioManager : MonoBehaviour
 
     int side = 1;
 
+    private void Start()
+    {
+        Musicsource.clip = side5;
+        Musicsource.Play();
+    }
+
     private void Update()
     {
+        Debug.Log(Physics.gravity);
+        if (Physics.gravity.x <= 10f && Physics.gravity.x >= 9f)
+        {
+            if(side != 6)
+            {
+                side = 6;
+                PlayMusic(side6);
+
+            }
+        }
+
+        if (Physics.gravity.x <= -9f && Physics.gravity.x >= -10f)
+        {
+            if(side != 1)
+            {
+                side = 1;
+                PlayMusic(side1);
+
+            }        
+        }
+        if (Physics.gravity.y >= 9f && Physics.gravity.y <= 10f)
+        {
+            if(side != 2)
+            {
+                side = 2;
+                PlayMusic(side2);
+
+            }        
+        }
+        if (Physics.gravity.y >= -10f && Physics.gravity.y <= -9f)
+        {
+            if(side != 3)
+            {
+                side = 3;
+                PlayMusic(side3);
+
+            }       
+            
+        }
+        if (Physics.gravity.z >= 9f && Physics.gravity.z <= 10f)
+        {
+            if(side != 4)
+            {
+                side = 4;
+                PlayMusic(side4);
+
+            }        
+        }
+
+        if (Physics.gravity.z >= -9f && Physics.gravity.z <= -10f)
+        {
+            if(side != 5)
+            {
+                side = 5;
+                PlayMusic(side5);
+
+            }        
+        }
         if (Input.GetKeyDown(KeyCode.Q)){
             source.PlayOneShot(switchGravity);
         }
@@ -49,6 +113,13 @@ public class AudioManager : MonoBehaviour
 
 
         
+    }
+
+    public void PlayMusic(AudioClip musicclip)
+    {
+        Musicsource.Stop();
+        Musicsource.clip = musicclip;
+        Musicsource.Play();
     }
 
 }
